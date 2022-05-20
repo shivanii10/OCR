7b5037eb56e58803da5ecdcfd061a72154a4236d
Shivani Surana <shivanisurana10@gmail.com>
2022-05-20 15:32:43 +0530

    Delete P directory

--- P/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^translated/$', views.translated, name='translated'),
-    url(r'^extracted/$', views.extracted, name='extracted'),
-
-]
